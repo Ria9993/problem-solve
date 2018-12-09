edd41785c3393f666d9b2bd58d33462439b4d11b
SiBeRiA9993 <hyunjun2372@gmail.com>
2018-12-09 15:44:42 +0900

    commit

--- A set of advanced ordered pairs.cpp
+++ A set of advanced ordered pairs.cpp
@@ -1,24 +1,20 @@
 #include <stdio.h>
 
-typedef struct _addr {
-    int addr;
-    int value;
-}_addr;
 typedef struct _pair {
-    _addr x;
-    _addr y;
-}_pair;
 
+}_pair;
+int num[5000][105]={0,};
+bool chk(int *x,int *y) {
+    bool flag=false;
+    for(int i=0;i<) //ing...
+    return flag;
+}
 int main() {
-    _pair pair[100];
-    _addr num[100][100];
-    int cnt[100]={0,};
-    int n=0;
-    for(int i=0;pair[i-1].x.value==100||pair[i-1].y.value==100;i++,n++) {
-        scanf("%d %d",&pair[i].x,&pair[i].y);
-        num[pair[i].x.value][cnt[pair[i].x.value]]={i,num[pair[i].x.value][cnt[pair[i].x.value]].value+1};    
-        
+    int pair[5000][10000],n;
+    for(int i=0;pair[i][0]!=100&&pair[i][1]!=100;i++,n++) {
+        scanf("%d %d",&pair[i][0],&pair[i][1]);
+        num[i][pair[i][0]]++;
+        num[i][pair[i][1]]++;
     }
-    
     return 0;
 }
---
+++ temp.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+struct NODE {
+    struct NODE *next;
+    int x;
+};
+typedef struct _addr {
+    int addr;
+    int value;
+}_addr;
+typedef struct _pair {
+    _addr x;
+    _addr y;
+}_pair;
+
+int main() {
+    _pair pair[100];
+    _addr num[100][100];
+    int cnt[100]={0,};
+    int n=0;
+    for(int i=0;pair[i-1].x.value==100||pair[i-1].y.value==100;i++,n++) {
+        scanf("%d %d",&pair[i].x,&pair[i].y);
+        num[pair[i].x.value][cnt[pair[i].x.value]]={i,num[pair[i].x.value][cnt[pair[i].x.value]].value+1};    
+
+    }
+    
+    return 0;
+}
