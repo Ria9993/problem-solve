537dc9a5fc038a20a7a82ff27c6530cdb45a114b
SiBeRiA9993 <hyunjun2372@gmail.com>
2018-12-22 19:59:15 +0900

    commit

--- 2018/12/knapsack.cpp
+++ 2018/12/knapsack.cpp
@@ -2,32 +2,32 @@
 #include <stdlib.h>
 #include <algorithm>
 using namespace std;
-typedef struct _gold{
+typedef struct _gold {
 	int w;
 	int c;
 	float m;
 }_gold;
 _gold gold[50000];
-bool cmp(_gold x,_gold y) {return x.m>y.m;}
+bool cmp(_gold x, _gold y) { return x.m>y.m; }
 int main() {
-	int n,m, now=0;
-	double res=0;
-	scanf("%d %d",&n,&m);
-	for(int i=0;i<n;i++) {
-		scanf("%d %d",&gold[i].w,&gold[i].c);
-		gold[i].m = (float)gold[i].c/gold[i].w;
+	int n, m, now = 0;
+	double res = 0;
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i<n; i++) {
+		scanf("%d %d", &gold[i].w, &gold[i].c);
+		gold[i].m = (float)gold[i].c / gold[i].w;
 	}
-	sort(gold,gold+n,cmp);
-	for(int i=0;now<m;i++) {
-		if(now+gold[i].w<=m){now+=gold[i].w;res+=gold[i].w*gold[i].c;}
+	sort(gold, gold + n, cmp);
+	for (int i = 0; now<m; i++) {
+		if (now + gold[i].w <= m) { now += gold[i].w; res += (double) gold[i].c; }
 		else {
-			for(int j=gold[i].w;j>0;j--) {
-				if(now+j<=m){now+=j;res+=j*gold[i].c;}
+			for (int j = gold[i].w; j>0; j--) {
+				if (now + j <= m) { now += j; res += (double)j * gold[i].m; break; }
 			}
-		printf("%.0lf",res);
-		return 0;
+			printf("%.0lf", res);
+			return 0;
 		}
 	}
-	printf("%.0lf",res);
+	printf("%.0lf", res);
 	return 0;
 }
