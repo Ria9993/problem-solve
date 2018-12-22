9c30998dcc0a719e8d7910be1fa34e2d2be500f3
SiBeRiA9993 <hyunjun2372@gmail.com>
2018-12-22 19:57:46 +0900

    commit

--- 2018/12/password.cpp
+++ 2018/12/password.cpp
@@ -15,16 +15,16 @@ int main() {
 		for (int j = 0; j<5; j++) {
 			if (arr[i][j] == ans[j])continue;
 			switch (arr[i][j]) {
-			case 1: if (as == 2 || as == 4)continue;
-			case 2: if (as == 1 || as == 5 || as == 3)continue;
-			case 3: if (as == 2 || as == 6)continue;
-			case 4: if (as == 1 || as == 5 || as == 7)continue;
-			case 5: if (as == 2 || as == 4 || as == 6 || as == 8)continue;
-			case 6: if (as == 3 || as == 5 || as == 9)continue;
-			case 7: if (as == 4 || as == 8)continue;
-			case 8: if (as == 5 || as == 7 || as == 9 || as == 0)continue;
-			case 9: if (as == 6 || as == 8)continue;
-			case 0: if (as == 8)continue;
+				case 1: if (as == 2 || as == 4)continue;
+				case 2: if (as == 1 || as == 5 || as == 3)continue;
+				case 3: if (as == 2 || as == 6)continue;
+				case 4: if (as == 1 || as == 5 || as == 7)continue;
+				case 5: if (as == 2 || as == 4 || as == 6 || as == 8)continue;
+				case 6: if (as == 3 || as == 5 || as == 9)continue;
+				case 7: if (as == 4 || as == 8)continue;
+				case 8: if (as == 5 || as == 7 || as == 9 || as == 0)continue;
+				case 9: if (as == 6 || as == 8)continue;
+				case 0: if (as == 8)continue;
 			}
 			flag = false;
 		}
