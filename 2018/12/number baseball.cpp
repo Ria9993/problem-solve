a4bb3000b52fc34f23c5b3855bbc505b2dcca2fc
SiBeRiA9993 <hyunjun2372@gmail.com>
2018-12-29 22:00:18 +0900

    commit

---
+++ 2018/12/number baseball.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//typedef struct
+int wei[10]={0,};
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++) {
+        int arr[4]={0.},s=0,b=0;
+        scanf("%1d %1d %1d",&arr[0],&arr[1],&arr[2]);
+        scanf("%d %d",&s,&b);
+    }
+    return 0;
+}
