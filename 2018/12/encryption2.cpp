5839fee2f9b69b57a59443464ef334b1bf55de1e
SiBeRiA9993 <hyunjun2372@gmail.com>
2018-12-30 11:47:36 +0900

    commit

--- 2018/12/encryption2.cpp
+++ 2018/12/encryption2.cpp
@@ -4,17 +4,29 @@
 using namespace std;
 
 int main() {
-	char str[110] = { 0, };
+	char str[1100] = { 0, };
+	bool flag = false;
 	scanf("%[^\n]", str);
 	int cnt = strlen(str);
-	for (int i = 0; i<6; i++) {
+	for (int i = 0; i<cnt; i++) if (str[i] == '*') flag = true;
+	if (flag) { //복호화
+		for (int j = 0; j < (cnt / 6) + (cnt % 6 ? 1 : 0); j++) {
+			for (int i = 0; i < 6; i++) {
+				switch (str[(i * (cnt/6)) + j]) {
+					case '*': printf(" "); break;
+					case '.': continue; break;
+					default: printf("%c", str[(i * (cnt / 6)) + j]); break;
+				}
+			}
+		}
+		return 0;
+	}
+	for (int i = 0; i<6; i++) { //암호화
 		for (int j = 0; j < (cnt / 6) + (cnt % 6 ? 1 : 0); j++) {
 			switch (str[(j * 6) + i]) {
-				case ' ' : printf("*"); break;
-				case '\0': printf("."); break;
-				case '*' : printf(" "); break;
-				case '.' : continue; break;
-				default  : printf("%c", str[(j * 6) + i]); break;
+			case ' ': printf("*"); break;
+			case '\0': printf("."); break;
+			default: printf("%c", str[(j * 6) + i]); break;
 			}
 		}
 	}
