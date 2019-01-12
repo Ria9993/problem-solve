ad7e0efaeb8db08d72657610367f4e18178b9c9e
SiBeRiA9993 <hyunjun2372@gmail.com>
2019-01-12 22:00:01 +0900

    commit

--- 2019/1/addition of real numbers.cpp
+++ 2019/1/addition of real numbers.cpp
@@ -5,6 +5,7 @@
 #define max(x,y) x>y?(x):(y)
 #define SP i-(100-strlen(num))
 #define EP i+(100-strlen(num2))
+
 using namespace std;
 char num[110], num2[110];
 char sum[210];
@@ -16,37 +17,51 @@ void chk(int n,char type) {
 			sum[n] = '0';
 			chk(n-1,'-');
 		} break;
-		case '-' : if (sum[n] == '0'-1) {
-			if (sum[n - 1] == 0) sum[n + 1] = '9';
-			else sum[n - 1]--;
-			sum[n] = '0';
+		case '-' : if(sum[n]== '0'-1){
+			if(sum[n])
 			chk(n+1,'+');
 		} break;
 	}
 }
 int main() {
+	bool sumFlag=false,flag=false; //sum의 부호, 연산종류 [true일시 '-']
 	int nlen=0, mlen=0;
 	while (true) {
-		char mi,flag=0;
-		num[0] = NULL;
+		flag=false;
+		num[0] = NULL; 	
 		scanf(" %[^-.]", num);
 		if (num[0] == NULL) {
-			scanf("%*1c"); flag = true;
+			scanf("%*1c"); flag = true; // 마이너스 연산
 		}
 		if (num[0] == '0') return 0;
 		scanf("%*1c %s", num2);
-		nlen = max(nlen, strlen(num));
+		nlen = max(nlen, strlen(num));	
 		mlen = max(mlen, strlen(num2));
 
-		for (int i = 100 - strlen(num); i<100 + strlen(num2); i++) {
-			sum[i] = ((sum[i] - (sum[i] != 0 ? '0' : 0) + (i < 100 ? num[SP] : num2[SP - strlen(num)]) - '0')) + '0';
-			if ((sum[i] - (sum[i] != 0 ? '0' : 0)) >= 10) {
-				sum[i] -= 10;
-				if (sum[i - 1] == 0) sum[i - 1] = '1';
-				else sum[i - 1]++;
-				chk(i - 1,'+');
-				if (sum[100 - nlen-1] != 0)nlen++;
-				//if (sum[100 + mlen] == 0)mlen--;
+		if(!flag) { //+
+			for (int i = 100 - strlen(num); i<100 + strlen(num2); i++) {
+				sum[i] = ((sum[i] - (sum[i] != 0 ? '0' : 0) + (i < 100 ? num[SP] : num2[SP - strlen(num)]) - '0')) + '0';
+				if ((sum[i] - (sum[i] != 0 ? '0' : 0)) >= 10) {
+					sum[i] -= 10;
+					if (sum[i - 1] == 0) sum[i - 1] = '1';
+					else sum[i - 1]++;
+					chk(i - 1,'+');
+					if (sum[100 - nlen-1] != 0)nlen++;
+					//if (sum[100 + mlen] == 0)mlen--;
+				}
+			}
+		}
+		else { //-
+			for(int i = 100 + strlen(num2)-1;i>=100-strlen(num);i--) {
+				sum[i] = ((sum[i] - (sum[i] != 0 ? '0' : 0) - (i < 100 ? num[SP] : num2[SP - strlen(num)]) - '0')) + '0';
+				if ((sum[i] - (sum[i] != 0 ? '0' : 0)) < 0) {
+					sum[i] += 10;
+					if (sum[i - 1] == 0) sumFlag=true;
+					else sum[i - 1]++;
+					chk(i - 1,'-');
+					if (sum[100 - nlen-1] != 0)nlen++;
+					//if (sum[100 + mlen] == 0)mlen--;
+				}
 			}
 		}
 		while(sum[100 + mlen -1] == '0'&&mlen!=1)mlen--;
