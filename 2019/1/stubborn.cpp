dcba54b7fbdd863b4f364ca5e3fe0a12e6facd40
SiBeRiA9993 <hyunjun2372@gmail.com>
2019-01-13 12:06:35 +0900

    commit

--- 2019/1/stubborn.cpp
+++ 2019/1/stubborn.cpp
@@ -8,7 +8,9 @@ int main() {
 	scanf("%d %d %d", &p1, &p2, &n);
 	for (; p1 <= p2; p1++) {
 		now = p1;
-		for (i = 0; now / 10 != 0;i++,now=tmp)for (tmp=1; now != 0; now /= 10)tmp *= now % 10 ? now % 10 : 1;
+		for (i = 0; now / 10 != 0;i++,now=tmp)
+            for (tmp=1; now != 0; now /= 10)
+                tmp *= now % 10 ? now % 10 : 1;
 		if (i == n)l[cnt++] = p1;
 	}
 	printf("%d\n", cnt);
