c0e693b5897436ed79b41e0e2dbe7c441c9c4290
SiBeRiA9993 <hyunjun2372@gmail.com>
2019-01-13 12:04:33 +0900

    commit

--- 2019/1/stubborn.cpp
+++ 2019/1/stubborn.cpp
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int p1,p2,n,now;
-    int tmp,next=1;
-    int l[1000],cnt=0;
-    scanf("%d %d %d",&p1,&p2,&n);
-    for(;p1<=p2;p1++) {
-        now=p1,tmp=1;
-        while(now/10!=0)for(;now!=0;now/10)tmp*=now%10;now=tmp;
-        if(now==n)l[cnt++]=p1;
-    }
-    printf("%d\n",cnt);
-    for(int i=0;i<cnt;i++)printf("%d\n",l[cnt]);
-    return 0;
+	int p1, p2, n, now;
+	int tmp, next = 1;
+	int l[1000], cnt = 0;
+	int i, j;
+	scanf("%d %d %d", &p1, &p2, &n);
+	for (; p1 <= p2; p1++) {
+		now = p1;
+		for (i = 0; now / 10 != 0;i++,now=tmp)for (tmp=1; now != 0; now /= 10)tmp *= now % 10 ? now % 10 : 1;
+		if (i == n)l[cnt++] = p1;
+	}
+	printf("%d\n", cnt);
+	for (i = 0; i<cnt; i++)printf("%d\n", l[i]);
+	return 0;
 }
