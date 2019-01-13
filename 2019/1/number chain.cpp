a6fbb0b9521f869c2da3aab9c27dc8c4d31b5177
SiBeRiA9993 <hyunjun2372@gmail.com>
2019-01-13 15:05:34 +0900

    commit

--- 2019/1/number chain.cpp
+++ 2019/1/number chain.cpp
@@ -1,4 +1,3 @@
-#pragma warning(disable:4996)
 #include <stdio.h>
 #include <string.h>
 #include <algorithm>
@@ -7,36 +6,36 @@ using namespace std;
 bool desSort(int x, int y) { return x>y; }
 int Bint(char *str) {
 	int sum = 0;
-	for (int i = 0; i<strlen(str); i++, sum *= 10)
-		sum += str[i] - '0';
-	return sum / 10;
+	for (int i = 0; i < strlen(str); i++)
+		sum = (sum*10)+str[i] - '0';
+	return sum;
 }
 char * Bstr(int n) {
 	static char str[15];
-	for (int i = 0; n != 0; n /= 10, i++) str[i] = n % 10 +'0';
+	for (int i = 0; n != 0; n /= 10, i++) str[i] = n % 10 + '0';
 	strrev(str);
 	return str;
 }
-bool chk(int arr[], int n, int els) {
-	for (int i = 0; arr[i]!=0; i++)
-		if (arr[i] == n&&i!=els)return true;
+bool chk(int arr[], int n, int els ,int len) {
+	for (int i = 0; i<len; i++)
+		if (arr[i] == n && i != els)return true;
 	return false;
 }
-bool chk(int arr[], int sint) {
+bool chk(int arr[], int sint, int len) {
 	int flag = 0;
-	for (int i = 0; arr[i] != 0; i++)flag += chk(arr, arr[i],i);
+	for (int i = 0; i<len; i++)flag += chk(arr, arr[i], i,len);
 	return (bool)flag;
 }
 int main() {
 	char str[15];
 	int sint;
-	int res[10000], cnt = 0,asc, des;
+	int res[10000], cnt = 0, asc, des;
 	memset(res, 0, sizeof(res));
 
 	scanf("%s", str);
 	sint = Bint(str);
 	res[cnt++] = sint;
-	
+
 	do {
 		sort(str, str + strlen(str), desSort);
 		des = Bint(str);
@@ -45,8 +44,8 @@ int main() {
 
 		res[cnt] = des - asc;
 		strcpy(str, Bstr(res[cnt++]));
-	} while(!chk(res,sint));
-	printf("%d\n", cnt-1);
-	for (int i = 0; i<cnt-1; i++)printf("%d\n", res[i]);
+		} while (!chk(res, sint,cnt));
+	printf("%d\n", cnt - 1);
+	for (int i = 0; i<cnt - 1; i++)printf("%d\n", res[i]);
 	return 0;
 }
