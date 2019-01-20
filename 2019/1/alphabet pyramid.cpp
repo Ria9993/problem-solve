5697292957f134ff26c3cf2e09e13f9333bc4930
SiBeRiA9993 <hyunjun2372@gmail.com>
2019-01-20 11:58:10 +0900

    commit

--- 2019/1/alphabet pyramid.cpp
+++ 2019/1/alphabet pyramid.cpp
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main() {
-    int n;
-    char ab;
-    scanf("%c",&ab);
-    scanf("%d",&n);
-    if(!isupper(ab)||n>24) printf("입력에러!");
-    for(int i=1;i<=n;i++) {
-        for(int j=i;j<=n;j++)printf(" ");
-        for(int j=1;j<=((i-1)*2)+1;j++) {
-            printf("%c",ab++);
-            if(ab>'Z')ab='A';
-        }
-        printf("\n");
-    }
-    return 0;
+	int n,len=0;
+	char ab, tstr[50] = { 0, };
+	scanf("%c", &ab);
+	scanf("%d", &n);
+	if (!isupper(ab) || n > 24) {printf("입력에러!");exit(0);}
+	for (int i = 0; i < n; i++) {
+		for (int j = i; j < n-1; j++)printf(" ");
+		for (int j = 1,len=0; j <= (i * 2) + 1; j++) {
+			tstr[len++] = ab++;
+			if (ab > 'Z')ab = 'A';
+		}
+		printf("%s", i % 2 ? tstr: strrev(tstr));
+		memset(tstr, 0, sizeof(tstr));
+		printf("\n");
+	}
+	return 0;
 }
