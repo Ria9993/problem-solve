2e7440864891f6f78d509e81c5b770e2ba193a57
SiBeRiA9993 <hyunjun2372@gmail.com>
2019-01-20 12:39:47 +0900

    commit

--- 2019/1/bassball game.cpp
+++ 2019/1/bassball game.cpp
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #define INF -1
 int main() {
-	int n,sum=0,maxP=INF,minP=INF;
-	double rate=0,max=INF,min=INF;
+	int n, sum = 0, maxP = INF, minP = INF;
+	double rate = 0, max = INF, min = INF;
 	char tmp;
 
 	scanf("%d", &n);
 	for (int i = 1; i <= n; i++) {
 		scanf(" %c", &tmp);
-		sum += tmp;
+		sum += tmp == 'W';
 		rate = (double)sum / i;
-		if (max == INF || rate >= max) {
+		if ((max == INF || rate >= max) && i >= 3) {
 			max = rate;
 			maxP = i;
 		}
-		if (min == INF || rate <= min) {
+		if ((min == INF || rate <= min) && i >= 3) {
 			min = rate;
 			minP = i;
 		}
