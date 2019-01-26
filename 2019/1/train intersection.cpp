4de6ed255d47400565cfe493b46cd6458b095c4f
SiBeRiA9993 <hyunjun2372@gmail.com>
2019-01-26 18:26:32 +0900

    commit

--- 2019/1/train intersection.cpp
+++ 2019/1/train intersection.cpp
@@ -7,6 +7,7 @@ typedef enum _OP{
 }_OP;
 _OP OP[100010];
 int opCnt=0;
+int n,m,cnt=1;
 int queue[100010]={0,},top=0;
 
 void push(int n) {queue[top++]=n;}
@@ -14,15 +15,16 @@ int  pop()       {return queue[--top];}
 int  view()      {return queue[top-1];}
 void sp(_OP n)   {OP[opCnt++]=n;}
 int main() {
-    int n,m,cnt=1;
     
     scanf("%d",&n);
     for(int i=0;i<n;i++) {
         scanf("%d",&m);
-             if (cnt==m)      {sp(S);sp(X); cnt++;       continue;}
-        else if (i==0)        {sp(S);       push(m);     continue;}
-        else if (view()==cnt) {sp(X);       pop();cnt++; continue;}
-           else               {sp(S);       push(m);              }
+             if (cnt==m)      {sp(S);sp(X); cnt++;  }
+        else if (i==0)        {sp(S);       push(m);}
+        else if (view()==cnt) {sp(X);       pop();cnt++;
+                               while(view()==cnt){sp(X);pop();cnt++;}
+                               sp(S);push(m);}
+           else               {sp(S);       push(m);}
     }
     while(top) {
 		if (queue[--top] != cnt++) { printf("IMPOSSIBLE"); exit(0); }
