cdcad969dceb723ccf2b719ec78b44c16d54d174
SiBeRiA9993 <hyunjun2372@gmail.com>
2019-01-26 18:37:38 +0900

    commit

--- 2019/1/ladder game.cpp
+++ 2019/1/ladder game.cpp
@@ -1,2 +1,20 @@
 #include <stdio.h>
 
+int main() {
+	int n, m;
+	int p[30] = { 0, };
+	for (int i = 0; i<30; i++)p[i] = i;
+
+	scanf("%d %d", &n, &m);
+
+	for (int i = 0; i<m; i++) {
+		int l;
+		scanf("%d", &l);
+		for (int j = 0; j<n; j++) {
+			if (p[j] == l - 1)p[j] = l;
+			else if (p[j] == l)p[j] = l - 1;
+		}
+	}
+	for (int i = 0; i<n; i++) printf("%d %c\n", i + 1, p[i] + 'A');
+	return 0;
+}
