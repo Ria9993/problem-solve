800dcf4a4c49a0bc7a2b10ea990b8de72b573337
SiBeRiA9993 <hyunjun2372@gmail.com>
2019-05-18 21:19:36 +0900

    commit

---
+++ 2019/5/select number.cpp
@@ -0,0 +1,13 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+int n,arr[100010];
+int main() {
+    scanf("%d",&n);
+    for(int i=0;i<n;i++) scanf("%d",&arr);
+    for(int i=0;i<n;i++) {
+
+
+
+    }
+    return 0;
+ }
